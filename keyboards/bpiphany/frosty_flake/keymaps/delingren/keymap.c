dcceededcd6c3988073fe2f4074477ce5e345d0d
Deling Ren <cookiejar.seattle@gmail.com>
2021-03-24 10:26:56 -0700

    Formatting frosty_flake

--- keyboards/bpiphany/frosty_flake/keymaps/delingren/keymap.c
+++ keyboards/bpiphany/frosty_flake/keymaps/delingren/keymap.c
@@ -6,6 +6,8 @@ enum keyboard_layers {
     _ML,
 };
 
+#define ML_CAPS LT(_ML,KC_CAPS)
+
 bool led_update_user(led_t usb_led) {
     writePin(C5, !usb_led.caps_lock);
     writePin(B7, !IS_LAYER_ON(_FL));
@@ -33,29 +35,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
 
 [0] = LAYOUT_tkl_ansi(
-    KC_ESC,                   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,      KC_PSCR,  KC_SLCK, KC_PWR,
-    KC_GRV,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,     KC_INS,   KC_HOME, KC_PGUP,
-    KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,     KC_DEL,   KC_END,  KC_PGDN,
-    LT(_ML,KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,
-    KC_LSHIFT,                KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSHIFT,             KC_UP,
-    KC_LCTL,         KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, KC_RALT,  MO(_FL), KC_RCTL,     KC_LEFT,  KC_DOWN, KC_RGHT
+    KC_ESC,             KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,      KC_PSCR,  KC_SLCK, KC_PWR,
+    KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,     KC_INS,   KC_HOME, KC_PGUP,
+    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,     KC_DEL,   KC_END,  KC_PGDN,
+    ML_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,
+    KC_LSHIFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSHIFT,             KC_UP,
+    KC_LCTL,   KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, KC_RALT,  MO(_FL), KC_RCTL,     KC_LEFT,  KC_DOWN, KC_RGHT
 ),
 
 [1] = LAYOUT_tkl_ansi(
-    RESET,            KC_BRID, KC_BRIU, _______, _______, BL_DEC,  BL_INC,  KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE,  KC_VOLD, KC_VOLU,      _______, _______, _______,
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,       _______, _______, _______,
-    _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,      _______, _______, _______,
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,           _______,
-    _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,               KC_PGUP,
-    _______, _______, _______,                            _______,                            _______, _______,  _______, _______,      KC_HOME, KC_PGDN, KC_END
+    RESET,              KC_BRID, KC_BRIU, _______, _______, BL_DEC,  BL_INC,  KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE,  KC_VOLD, KC_VOLU,      _______, _______, _______,
+    KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,       _______, _______, _______,
+    _______,   _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,      _______, _______, _______,
+    _______,   KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,           _______,
+    _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,               KC_PGUP,
+    _______,   _______, _______,                            _______,                            _______, _______,  _______, _______,      KC_HOME, KC_PGDN, KC_END
 ),
 
 [2] = LAYOUT_tkl_ansi(
-    _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,      _______, _______, _______,
-    KC_ESC,  KC_BRID, KC_BRIU, _______, _______, BL_DEC,  BL_INC,  KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD,  KC_VOLU, KC_DEL,       _______, _______, _______,
-    _______, _______, KC_UP,   _______, _______, _______, _______, _______,   KC_UP, _______, _______, KC_HOME,  KC_END,  _______,      _______, _______, _______,
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN,           _______,
-    _______,          _______, _______, _______, _______, _______, _______, KC_END,  _______, KC_PGDN, _______,           _______,               KC_PGUP,
-    _______, _______, _______,                            _______,                            _______, _______,  _______, _______,      KC_HOME, KC_PGDN, KC_END
+    _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,      _______, _______, _______,
+    KC_ESC,    KC_BRID, KC_BRIU, _______, _______, BL_DEC,  BL_INC,  KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD,  KC_VOLU, KC_DEL,       _______, _______, _______,
+    _______,   _______, KC_UP,   _______, _______, _______, _______, _______,   KC_UP, _______, _______, KC_HOME,  KC_END,  _______,      _______, _______, _______,
+    _______,   KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN,           _______,
+    _______,            _______, _______, _______, _______, _______, _______, KC_END,  _______, KC_PGDN, _______,           _______,               KC_PGUP,
+    _______,   _______, _______,                            _______,                            _______, _______,  _______, _______,      KC_HOME, KC_PGDN, KC_END
 ),
 };
