616a616db0598a74691f5a00287987e68d93ba23
Deling Ren <deling@Hack.lan>
2021-03-28 00:11:53 -0700

    Refactor clueboard 60

--- keyboards/clueboard/60/keymaps/delingren/keymap.c
+++ keyboards/clueboard/60/keymaps/delingren/keymap.c
@@ -7,36 +7,38 @@ enum keyboard_layers {
     _CL
 };
 
+#define ML_CAPS LT(_ML,KC_CAPS)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Layer 0: Default Layer
     * ,-----------------------------------------------------------.
-    * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  `|BSp|
+    * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  BSpc |
     * |-----------------------------------------------------------|
     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
     * |-----------------------------------------------------------|
     * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
     * |-----------------------------------------------------------|
-    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn0|
+    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|  Shift   |
     * |-----------------------------------------------------------'
-    * |Ctrl |Gui|Alt  |         Space         |Alt  |Gui|Fn |Ctrl |
+    * |Ctrl |Alt|Gui  |         Space         |Gui  |ML |FL |Ctrl |
     * `-----------------------------------------------------------'
     */
-[_BL] = LAYOUT_all(
-    KC_GESC,         KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_NO, KC_BSPC, \
-    KC_TAB,          KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,       KC_BSLS, \
-    LT(_ML,KC_CAPS), KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,  KC_ENT, \
-    KC_LSFT,  KC_NO, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_NO, \
-    KC_LCTL,         KC_LALT, KC_LGUI,                        KC_SPC,                  KC_RGUI, KC_RALT, MO(_FL), KC_RCTL),
-[_FL] = LAYOUT_all(
-    RESET,            KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12, _______, KC_DEL,   \
-    _______,          _______, KC_UP,   _______, _______, _______, _______, _______, KC_UP,  _______, _______, KC_HOME,  KC_END,           _______, \
-    _______,          KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN, _______,  _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______,          _______,_______,                        _______,                     _______,  _______,  _______,  _______),
-[_ML] = LAYOUT_all(
-    KC_GRV,           KC_BRID, KC_BRIU, KC_NO,   KC_NO,   BL_DEC,  BL_INC,  KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_DEL,  \
-    _______,          _______, KC_UP,   _______, _______, _______, _______, _______, KC_UP,   _______, _______, KC_HOME, KC_END,           _______, \
-    _______,          KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN, _______,  _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______,          _______,_______,                        _______,                        _______,  _______,  _______,  _______),
+[_BL] = LAYOUT_60_ansi(
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+    ML_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LCTL, KC_LALT, KC_LGUI,                        KC_SPC,                        KC_RGUI, MO(_ML), MO(_FL), KC_RCTL),
+[_FL] = LAYOUT_60_ansi(
+    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12, KC_DEL,
+    _______, _______, KC_UP,   _______, _______, _______, _______, _______, KC_UP,   _______, _______, KC_HOME,  KC_END, _______,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______,_______,                        _______,                     _______,  _______,  _______,  _______),
+[_ML] = LAYOUT_60_ansi(
+    KC_GRV,  KC_BRID, KC_BRIU, KC_NO,   KC_NO,   BL_DEC,  BL_INC,  KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU, KC_DEL,
+    _______, _______, KC_UP,   _______, _______, _______, _______, _______, KC_UP,   _______, _______, KC_HOME, KC_END,  _______,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______,_______,                        _______,                        _______, _______,  _______,  _______),
 };
