831d6256391891ec568b1a03fe9becf6e705e1db
Deling Ren <cookiejar.seattle@gmail.com>
2021-04-01 00:19:37 -0700

    change capslock to fl on 3 boards: mf68, clueboard, and kbd67

--- keyboards/clueboard/60/keymaps/delingren/keymap.c
+++ keyboards/clueboard/60/keymaps/delingren/keymap.c
@@ -3,11 +3,10 @@
 enum keyboard_layers {
     _BL,
     _FL,
-    _ML,
-    _CL
+    _ML
 };
 
-#define ML_CAPS LT(_ML,KC_CAPS)
+#define FL_CAPS LT(_FL,KC_CAPS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Layer 0: Default Layer
@@ -26,19 +25,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BL] = LAYOUT_60_ansi(
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-    ML_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+    FL_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     KC_LCTL, KC_LALT, KC_LGUI,                        KC_SPC,                        KC_RGUI, MO(_ML), MO(_FL), KC_RCTL),
 [_FL] = LAYOUT_60_ansi(
-    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12, KC_DEL,
-    _______, _______, KC_UP,   _______, _______, _______, _______, _______, KC_UP,   _______, _______, KC_HOME,  KC_END, _______,
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______,_______,                        _______,                     _______,  _______,  _______,  _______),
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_DEL,
+    _______, _______, _______, _______, RESET,   _______, _______, _______, KC_UP,   _______, _______, KC_HOME, KC_END, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,
+    _______, _______,_______,                        _______,                        _______, _______, _______, _______),
 [_ML] = LAYOUT_60_ansi(
     KC_GRV,  KC_BRID, KC_BRIU, KC_NO,   KC_NO,   BL_DEC,  BL_INC,  KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU, KC_DEL,
-    _______, _______, KC_UP,   _______, _______, _______, _______, _______, KC_UP,   _______, _______, KC_HOME, KC_END,  _______,
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, KC_HOME, KC_END,  _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______,_______,                        _______,                        _______, _______,  _______,  _______),
+    _______, _______, _______,                            _______,                   _______, _______, _______, _______),
 };
