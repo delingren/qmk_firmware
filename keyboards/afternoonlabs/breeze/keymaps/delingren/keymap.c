5b9c993d054b145b13625d41e60e5f17998ab5b2
Deling Ren <deling@Hack.local>
2021-07-24 22:25:11 -0700

    breeze: caps

--- keyboards/afternoonlabs/breeze/keymaps/delingren/keymap.c
+++ keyboards/afternoonlabs/breeze/keymaps/delingren/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,   KC_LBRC, KC_RBRC, KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┤
-     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_UP,
+     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_UP,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT,  KC_LEFT, KC_DOWN, KC_RIGHT,
   //└────────┴────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┴────────┴────────┴────────┘└────────┴────────┴────────┘
@@ -72,3 +72,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                           └────────┴────────┴────────┴────────┘        └────────┴────────┴────────┴────────┘
   )
 };
+
+void keyboard_pre_init_user() {
+    setPinOutput(B0);
+    writePinHigh(B0);
+    setPinOutput(D5);
+    writePinHigh(D5);
+}
+
+bool led_update_user(led_t usb_led) {
+    writePin(B0, !usb_led.caps_lock);
+    writePin(D5, !usb_led.caps_lock);
+
+    return false;
+}
