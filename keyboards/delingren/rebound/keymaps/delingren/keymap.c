3d9d94d268163c1075b46f483238872d9b327514
Deling Ren <cookiejar.seattle@gmail.com>
2021-08-13 14:04:04 -0700

    some rebound changes and kbd67split changes

--- keyboards/delingren/rebound/keymaps/delingren/keymap.c
+++ keyboards/delingren/rebound/keymaps/delingren/keymap.c
@@ -9,12 +9,15 @@ enum layer_names {
   _BASE,
   _LOWER,
   _RAISE,
-  _ADJUST
+  _ADJUST,
+  _NUM
 };
 
 #define LOWER  MO(_LOWER)
 #define RAISE  MO(_RAISE)
 #define ADJUST MO(_ADJUST)
+#define TG_NUM TG(_NUM)
+#define MO_NUM MO(_NUM)
 
 #define LT_ENT LT(LOWER, KC_ENT)
 #define LT_SPC LT(RAISE, KC_SPC)
@@ -25,14 +28,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_ESC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-    KC_LCTL, KC_LALT, KC_LGUI, LOWER,            LT_ENT, KC_MUTE, LT_SPC,           RAISE,   KC_RGUI, KC_RALT, KC_RCTL
+    KC_LCTL, LOWER,   KC_LALT, KC_LGUI,          LT_ENT, KC_MUTE, LT_SPC,           KC_RGUI, KC_RALT, RAISE,   KC_RCTL
 ),
 
 [_LOWER] = LAYOUT_all(
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
     KC_GRV,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_UNDS, KC_PLUS,
     KC_TILD, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, _______, KC_HOME, KC_END,  KC_PGUP, KC_PGDN, XXXXXXX, _______,
-    _______, _______, _______, _______,          _______, _______, _______,          _______, _______, _______, _______
+    MO_NUM,  _______, _______, _______,          _______, _______, _______,          _______, _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT_all(
@@ -48,11 +51,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
+
+[_NUM] = LAYOUT_all(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_PSLS, KC_BSPC,
+    TG_NUM,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PAST, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG_NUM,  KC_P1,   KC_P2,   KC_P3,   KC_PENT, XXXXXXX, XXXXXXX,
+    _______, _______, _______, _______,          _______, _______, KC_P0,            KC_PDOT, _______, _______, _______
+),
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_270; }
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (clockwise) {
@@ -63,13 +75,155 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     return true;
 }
 
+static bool caps_lock_on = false;
+
 void oled_task_user(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    static const char PROGMEM hr[] = {
+        0x81, 0x81, 0x81, 0x81, 
+        0x00,
     };
-    oled_write_P(qmk_logo, false);
+
+    static const char PROGMEM logo[] = {
+        0x80, 0x84, 0x85, 0x80,
+        0x80, 0x86, 0x87, 0x80,
+        0x00,
+    };
+
+    static const char PROGMEM empty [] = {
+        0x80, 0x80, 0x80, 0x80,
+        0x80, 0x80, 0x80, 0x80,
+        0x00,
+    };
+
+    static const char PROGMEM layer0[] = {
+        0x90, 0x91, 0x80, 0x80,
+        0x92, 0x93, 0x80, 0x80,
+        0x00,
+    }; 
+
+    static const char PROGMEM layer1[] = {
+        0x80, 0x80, 0x94, 0x95, 
+        0x80, 0x80, 0x96, 0x97, 
+        0x00,
+    }; 
+
+    static const char PROGMEM layer01[] = {
+        0x90, 0x91, 0x94, 0x95, 
+        0x92, 0x93, 0x96, 0x97, 
+        0x00,
+    }; 
+
+    static const char PROGMEM layer2[] = {
+        0x98, 0x99, 0x80, 0x80,
+        0x9a, 0x9b, 0x80, 0x80,
+        0x00,
+    }; 
+
+    static const char PROGMEM layer3[] = {
+        0x80, 0x80, 0x9c, 0x9d, 
+        0x80, 0x80, 0x9e, 0x9f, 
+        0x00,
+    }; 
+
+    static const char PROGMEM layer23[] = {
+        0x98, 0x99, 0x9c, 0x9d, 
+        0x9a, 0x9b, 0x9e, 0x9f, 
+        0x00,
+    }; 
+
+    static const char PROGMEM mod0[] = {
+        0xa0, 0xa1, 0x80, 0x80,
+        0xa2, 0xa3, 0x80, 0x80,
+        0x00,
+    }; 
+
+    static const char PROGMEM mod1[] = {
+        0x80, 0x80, 0xa4, 0xa5, 
+        0x80, 0x80, 0xa6, 0xa7, 
+        0x00,
+    }; 
+
+    static const char PROGMEM mod01[] = {
+        0xa0, 0xa1, 0xa4, 0xa5, 
+        0xa2, 0xa3, 0xa6, 0xa7, 
+        0x00,
+    }; 
+
+    static const char PROGMEM mod2[] = {
+        0xa8, 0xa9, 0x80, 0x80,
+        0xaa, 0xab, 0x80, 0x80,
+        0x00,
+    }; 
+
+    static const char PROGMEM mod3[] = {
+        0x80, 0x80, 0xac, 0xad, 
+        0x80, 0x80, 0xae, 0xaf, 
+        0x00,
+    }; 
+
+    static const char PROGMEM mod23[] = {
+        0xa8, 0xa9, 0xac, 0xad, 
+        0xaa, 0xab, 0xae, 0xaf, 
+        0x00,
+    }; 
+
+    static const char PROGMEM capslock[] = {
+        0x80, 0x88, 0x89, 0x80,
+        0x80, 0x8a, 0x8b, 0x80,
+        0x00,
+    };
+
+    oled_write_P(logo, false);
+    oled_write_P(hr, false);
+
+    bool layer0_on = IS_LAYER_ON(0);
+    bool layer1_on = IS_LAYER_ON(1);
+    bool layer2_on = IS_LAYER_ON(2);
+    bool layer3_on = IS_LAYER_ON(3);
+
+    // we are not doing one shot mods here
+    static uint8_t mods;
+    mods = get_mods();
+    bool mod0_on = mods & MOD_MASK_CTRL;
+    bool mod1_on = mods & MOD_MASK_SHIFT;
+    bool mod2_on = mods & MOD_MASK_ALT;
+    bool mod3_on = mods & MOD_MASK_GUI;
+
+    if (layer0_on) {
+        oled_write_P(layer1_on ? layer01 : layer0, false);
+    } else {
+        oled_write_P(layer1_on ? layer1 : empty, false);
+    }
+
+    if (layer2_on) {
+        oled_write_P(layer3_on ? layer23 : layer2, false);
+    } else {
+        oled_write_P(layer3_on ? layer3 : empty, false);
+    }
+
+    oled_write_P(hr, false);
+
+    if (mod0_on) {
+        oled_write_P(mod1_on ? mod01 : mod0, false);
+    } else {
+        oled_write_P(mod1_on ? mod1 : empty, false);
+    }
+
+    if (mod2_on) {
+        oled_write_P(mod3_on ? mod23 : mod2, false);
+    } else {
+        oled_write_P(mod3_on ? mod3 : empty, false);
+    }
+
+    oled_write_P(hr, false);
+    oled_write_P(caps_lock_on ? capslock : empty, false);
 }
 
-void keyboard_post_init_user(void) { rgblight_sethsv(HSV_BLUE); }
+bool led_update_user(led_t led_state) {
+    caps_lock_on = led_state.caps_lock;
+    return true;
+}
+
+void keyboard_post_init_user(void) { 
+    rgblight_sethsv(HSV_BLUE); 
+}
