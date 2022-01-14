e73982205ba7ad355847b28b54fe6ab2ef4443a8
Deling Ren <cookiejar.seattle@gmail.com>
2022-01-13 22:28:29 -0800

    dz60

--- keyboards/delingren/dz60/keymaps/delingren/keymap.c
+++ keyboards/delingren/dz60/keymaps/delingren/keymap.c
@@ -15,43 +15,29 @@ enum keyboard_layers {
 
 #define FL_CAPS LT(_ADJUST,KC_CAPS)
 
-#define LAYOUT_60_ansi_split_space( \
-    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D, \
-    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A,           K3D, \
-    K40, K41, K42,           K44, K45, K46,      K48, K49,      K4B, K4D  \
-) { \
-    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D }, \
-    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D }, \
-    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, XXX, K2D }, \
-    { K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, XXX, XXX, K3D }, \
-    { K40, K41, K42, XXX, K44, K45, K46, XXX, K48, K49, XXX, K4B, XXX, K4D }  \
-}
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_BL] = LAYOUT_60_ansi_split_space(
+[_BL] = LAYOUT_60_ansi_split(
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
     FL_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,   \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT,  \
     KC_LCTL, KC_LALT, KC_LGUI,          LT_ENT,  ADJUST,  LT_SPC,                    KC_APP,  KC_RGUI,          KC_RALT, KC_RCTL),
 
-[_LOWER] = LAYOUT_60_ansi_split_space(
+[_LOWER] = LAYOUT_60_ansi_split(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, _______, _______, \
     KC_GRV,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, \
     KC_TILD, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______,                   _______, \
     _______, _______, _______,          _______, _______, _______,                   _______, _______,          _______, _______),
 
-[_RAISE] = LAYOUT_60_ansi_split_space(
+[_RAISE] = LAYOUT_60_ansi_split(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, \
     _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______,                   _______, \
     _______, _______, _______,          _______, _______, _______,                   _______, _______,          _______, _______),
 
-[_ADJUST] = LAYOUT_60_ansi_split_space(
+[_ADJUST] = LAYOUT_60_ansi_split(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
     _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, \
