2fb2feb6f707c82835b16d734e3bbb9fc532f7a0
Deling Ren <cookiejar.seattle@gmail.com>
2022-01-18 14:18:01 -0800

    Some changes to prime_e

--- keyboards/delingren/prime_e/keymaps/delingren/keymap.c
+++ keyboards/delingren/prime_e/keymaps/delingren/keymap.c
@@ -20,6 +20,7 @@ enum layer_names {
   _LOWER,
   _RAISE,
   _ADJUST,
+  _NUM,
 };
 
 #define LOWER  MO(_LOWER)
@@ -31,26 +32,32 @@ enum layer_names {
 #define SFT_ENT RSFT_T(KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_BASE] = LAYOUT(
-	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_DEL,
-	KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_ESC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT,
-	KC_LCTL, KC_LALT,          KC_LGUI,          LT_ENT,     LT_SPC,           KC_RGUI, KC_RALT,          KC_RCTL
-),
+  [_BASE] = LAYOUT(
+  	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_DEL,
+  	KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_ESC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  	KC_LCTL, KC_LALT,          KC_LGUI,          LT_ENT,     LT_SPC,           KC_RGUI,          KC_RALT, KC_RCTL
+  ),
 
-[_LOWER] = LAYOUT(
-	_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC,
-	KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_UNDS, KC_PLUS,
-	KC_GRV,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,    _______, KC_HOME, KC_END,  KC_PGUP, KC_PGDN, XXXXXXX, _______,
-	MO_NUM,  _______,          _______,          _______,    _______,          _______, _______,          _______
-),
+  [_LOWER] = LAYOUT(
+  	_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC,
+  	KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_UNDS, KC_PLUS,
+  	KC_GRV,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,    KC_BSPC, KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______, _______,
+  	_______, _______,          _______,          _______,    _______,          _______,          _______, _______
+  ),
 
- [_RAISE] = LAYOUT(
-	KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  _______,
-	KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
-	KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     _______, KC_HOME, KC_END,  KC_PGUP, KC_PGDN, XXXXXXX, _______,
-	_______, _______,          _______,          _______,	 _______,          _______, _______,          _______
-),
+   [_RAISE] = LAYOUT(
+  	KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  _______,
+  	KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+  	KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_DEL,  KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______, _______,
+  	_______, _______,          _______,          _______,	   _______,          _______,          _______, _______
+  ),
+  [_ADJUST] = LAYOUT(
+    XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE, XXXXXXX,
+    _______, _______,          _______,          _______,    _______,          _______,          _______, _______
+  ),
 };
 
 #ifdef RGBLIGHT_ENABLE
@@ -88,3 +95,6 @@ void led_set_user(uint8_t usb_led) {
   }
 }
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
