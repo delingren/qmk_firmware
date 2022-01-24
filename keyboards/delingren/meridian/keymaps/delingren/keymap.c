29c327f918f31ed470aae569bf3fc3f27d3b2ce3
Deling Ren <cookiejar.seattle@gmail.com>
2022-01-23 22:06:51 -0800

    Some updates

--- keyboards/delingren/meridian/keymaps/delingren/keymap.c
+++ keyboards/delingren/meridian/keymaps/delingren/keymap.c
@@ -80,8 +80,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void keyboard_post_init_user(void) {
 	rgblight_setrgb_at(0, 0, 0, 0); // [..., 0] = top LED
-    rgblight_setrgb_at(0, 0, 0, 1); // [..., 1] = middle LED
-    rgblight_setrgb_at(0, 0, 0, 2); // [..., 2] = bottom LED
+  rgblight_setrgb_at(0, 0, 0, 1); // [..., 1] = middle LED
+  rgblight_setrgb_at(0, 0, 0, 2); // [..., 2] = bottom LED
 }
 
 void led_set_user(uint8_t usb_led) {
