b9d4f63832ad1898da4172c2e066daa1f985780d
Deling Ren <cookiejar.seattle@gmail.com>
2022-03-21 21:44:20 -0700

    Prime_e

--- keyboards/delingren/prime_e/keymaps/delingren/keymap.c
+++ keyboards/delingren/prime_e/keymaps/delingren/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_DEL,
 	KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-	KC_LCTL, KC_LALT,          KC_LGUI,          LT_ENT,     LT_SPC,           KC_APP,                    KC_RALT, ADJUST ),
+	KC_LCTL, KC_LGUI,          KC_LALT,          LT_ENT,     LT_SPC,           KC_APP,                    KC_RALT, ADJUST ),
 
 [_LOWER] = LAYOUT(
 	KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC,
