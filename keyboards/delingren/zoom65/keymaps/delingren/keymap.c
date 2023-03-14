c2bde6f8ec75e08ec314b78da40e1d8f804431ea
delingren <cookiejar.seattle@gmail.com>
2023-03-14 11:03:30 -0700

    Update zoom65 to be data driven

--- keyboards/delingren/zoom65/keymaps/delingren/keymap.c
+++ keyboards/delingren/zoom65/keymaps/delingren/keymap.c
@@ -14,31 +14,36 @@ enum keyboard_layers {
 #define LT_SPC LT(RAISE, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_BL] = LAYOUT_65_ansi_blocker_split_space(
+[_BL] = LAYOUT_ansi_split_space(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_MUTE, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,  \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_HOME, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_END,  \
   KC_LCTL, KC_LGUI, KC_LALT,          LT_ENT,  ADJUST,  LT_SPC,                    KC_APP,  KC_RALT,          KC_LEFT, KC_DOWN, KC_RGHT  ),
 
-[_LOWER] = LAYOUT_65_ansi_blocker_split_space(
+[_LOWER] = LAYOUT_ansi_split_space(
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______, \
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, _______, _______, _______, \
   KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, KC_PGUP, \
   KC_GRV,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______, _______,          KC_PGUP, KC_PGDN, \
   _______, _______, _______,          _______, _______, _______,                   _______, _______,          KC_HOME, KC_PGDN, KC_END   ),
 
-[_RAISE] = LAYOUT_65_ansi_blocker_split_space(
+[_RAISE] = LAYOUT_ansi_split_space(
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______, \
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______, _______, \
   KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, KC_PGUP, \
   _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______, _______,          KC_PGUP, KC_PGDN, \
   _______, _______, _______,          _______, _______, _______,                   _______, _______,          KC_HOME, KC_PGDN, KC_END   ),
 
-[_ADJUST] = LAYOUT_65_ansi_blocker_split_space(
+[_ADJUST] = LAYOUT_ansi_split_space(
   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   KC_CAPS, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, KC_PGUP, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,          KC_PGUP, KC_PGDN, \
-  _______, _______, _______,          _______, _______, _______,                   _______, _______,          KC_HOME, KC_PGDN, KC_END   ),
+  _______, _______, _______,          KC_ENT,  _______, KC_SPC,                    _______, _______,          KC_HOME, KC_PGDN, KC_END   ),
 };
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+	tap_code_delay(clockwise ? KC_VOLD : KC_VOLU, 10);
+    return true;
+}
