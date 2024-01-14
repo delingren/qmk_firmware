2a21ba5f45953eace6b9b87ef85d8e16ebc925d0
Deling Ren <cookiejar.seattle@gmail.com>
2024-01-13 21:27:40 -0800

    HP laptop keyboard: adjust order

--- keyboards/delingren/hp/keymaps/delingren/keymap.c
+++ keyboards/delingren/hp/keymaps/delingren/keymap.c
@@ -9,20 +9,11 @@ enum keyboard_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT_ansi(
-                 KC_RSFT,                   KC_LSFT,
-                                   KC_RCTL,          KC_LCTL,
-        KC_F7,   KC_W,    KC_2,    KC_S,    KC_X,    KC_F6,   KC_F5,   KC_F8,
-        KC_ESC,  KC_Q,    KC_1,    KC_A,    KC_Z,    KC_TAB,  KC_TILD, KC_CAPS,
-        KC_F4,   KC_E,    KC_3,    KC_D,    KC_C,    KC_F3,   KC_F2,   KC_F1,
-        KC_G,    KC_R,    KC_4,    KC_F,    KC_V,    KC_T,    KC_5,    KC_B, 
-        KC_H,    KC_U,    KC_7,    KC_J,    KC_M,    KC_Y,    KC_6,    KC_N,
-                 KC_I,    KC_8,    KC_K,    KC_COMM, KC_RBRC,
-        KC_LGUI,
-                          KC_LALT,                                     KC_RALT,
-        KC_BSPC, KC_UP,   KC_DOWN, KC_ENT,  KC_SPC,           KC_BSLS, KC_PSCR,
-        KC_F11,  KC_0,    KC_9,    KC_L,    KC_DOT,  KC_F10,  KC_F9,   KC_F12,
-        KC_QUOT, KC_P,    KC_0,    KC_SCLN, KC_SLSH, KC_LBRC, KC_MINS, KC_EQL,
-        KC_INS,  KC_LEFT, KC_RGHT, KC_PGDN, KC_END,  KC_HOME, KC_PGUP, KC_DEL,
-                                                              LOWER
+      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_PSCR, KC_DEL,
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
+      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_END,
+      KC_LCTL, LOWER,   KC_LGUI, KC_LALT, KC_SPC,  KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 };
