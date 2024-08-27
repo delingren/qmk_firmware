45cf24866d77420fcbcfff87252b6ec5bb25c6e0
delingren <cookiejar.seattle@gmail.com>
2024-08-27 14:54:21 -0700

    Add alt/option to modifier keys for GESC

--- quantum/process_keycode/process_grave_esc.c
+++ quantum/process_keycode/process_grave_esc.c
@@ -26,7 +26,7 @@ static bool grave_esc_was_shifted = false;
 bool process_grave_esc(uint16_t keycode, keyrecord_t *record) {
     if (keycode == QK_GRAVE_ESCAPE) {
         const uint8_t mods    = get_mods();
-        uint8_t       shifted = mods & MOD_MASK_SG;
+        uint8_t       shifted = mods & MOD_MASK_SAG;
 
 #ifdef GRAVE_ESC_ALT_OVERRIDE
         // if ALT is pressed, ESC is always sent
